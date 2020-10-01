d3a81415f9cb31c6708afabc5f68c9406c5689d0
Kristjan <kristjan.psennoi@hotmail.com>
2020-10-01 23:48:15 +0300

    second commit

--- server.js
+++ server.js
@@ -12,29 +12,87 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', ejs);
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+
+    res.render('index.ejs', {
+        country: "",
+        name: "",
+        topLevelDomain: "",
+        callingCode: "",
+        capital: "",
+        region: "",
+        subRegion: "",
+        population: "",
+        timezone: "",
+        language: "",
+        currency: "",
+        flag: "",
+        errorMessage: ""
+    })
 });
 
 app.post('/', (req, res) => {
     let country;
+
     switch(req.body.countries){
         case '1': // estonia
-            country = estonia;
+            country = "estonia";
             break;
         case '2': // latvia
-            country = latvia;
+            country = "latvia";
             break;
         case '3': // lithuania
-            country = lithuania;
+            country = "lithuania";
+            break;
+        default:
+            country = "error";
             break;
     }
 
-    url = 'https://restcountries.eu/rest/v2/name/' + country + '?fullText=true '
-    axios.get(url).then(function(api){
-        //todo
-    }).catch(function(error){
-        console.log(error);
-    });
+    if(country == "error"){
+        
+        res.render('index.ejs', {
+            country: "",
+            name: "",
+            topLevelDomain: "",
+            callingCode: "",
+            capital: "",
+            region: "",
+            subRegion: "",
+            population: "",
+            timezone: "",
+            language: "",
+            currency: "",
+            flag: "",
+            errorMessage: "Please select a country."
+        })
+        
+    } else {
+
+        url = 'https://restcountries.eu/rest/v2/name/' + country + '?fullText=true '
+        axios.get(url).then(function(api){
+            let json = api.data;
+
+            res.render('index.ejs', {
+                country: country,
+                name: json[0].name,
+                topLevelDomain: json[0].topLevelDomain[0],
+                callingCode: "+" + json[0].callingCodes[0],
+                capital: json[0].capital,
+                region: json[0].region,
+                subRegion: json[0].subregion,
+                population: json[0].population,
+                timezone: json[0].timezones[0],
+                language: json[0].languages[0].name,
+                currency: json[0].currencies[0].name + " (" + json[0].currencies[0].code + " " + json[0].currencies[0].symbol + ")",
+                flag: json[0].flag,
+                errorMessage: ""
+            });
+        }).catch(function(error){
+            console.log(error);
+        });
+    }
+
+
 });
 
 let port = 4477;
